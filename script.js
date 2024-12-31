774dbe022f7914a17dd7e885dd81357a09c5686d
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-01-01 00:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+	function Person(name, age) {
+		this.name = name;
+		this.age = age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	function greet(){
+		Console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
+
+	
+}
+
+class Employee extends Person {
+	function Employee(name, age, jobTitle) {
+		super(name, age)
+		this.jobTitle = jobTitle;
+	}
+
+	function jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
+}
+
+
 
 // Do not change code below this line
 window.Person = Person;
